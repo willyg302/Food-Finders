40c461e630b069f588f3634fcbf64cbf27c81612
William Gaul <willyg302@gmail.com>
2014-03-03 21:33:25 -1000

    Bare bones Bootstrap layout

---
+++ packages/bootstrap/package.js
@@ -0,0 +1,18 @@
+// Bootstrap 3.1.1 shiv for Meteor, since its default is 2.3.0
+
+Package.describe({
+	summary: "Load Bootstrap 3.1.1 script"
+});
+
+Package.on_use(function (api) {
+	api.use(['jquery'], 'client');
+	api.add_files(['../../public/bootstrap/fonts/glyphicons-halflings-regular.eot'], 'client');
+	api.add_files(['../../public/bootstrap/fonts/glyphicons-halflings-regular.svg'], 'client');
+	api.add_files(['../../public/bootstrap/fonts/glyphicons-halflings-regular.ttf'], 'client');
+	api.add_files(['../../public/bootstrap/fonts/glyphicons-halflings-regular.woff'], 'client');
+	api.add_files(['../../public/bootstrap/js/bootstrap.min.js'], 'client');
+	api.add_files(['../../public/bootstrap/css/bootstrap.min.css'], 'client');
+
+	// Font path override
+	api.add_files('path-override.css', 'client');
+});
