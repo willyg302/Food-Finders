8f6a2645c523c9c2d84e76890821f3aa104b72e2
William Gaul <willyg302@gmail.com>
2014-03-09 21:25:41 -1000

    Starting on individual restaurant pages
    Yeah so doesn't work. Routing is broken. But will eventually.

--- client/food-finders.js
+++ client/food-finders.js
@@ -3,6 +3,14 @@ Restaurants = new Meteor.Collection("restaurants");
 
 var restaurantsHandle = Meteor.subscribe("restaurants");
 
+// ID of currently selected restaurant
+Session.setDefault('restaurant_id', null);
+
+
+Template.mainPanel.mainview = function() {
+	return !Session.get('restaurant_id');
+};
+
 
 Template.restaurantList.restaurants = function() {
 	all = Restaurants.find({}).fetch();
@@ -24,4 +32,34 @@ Template.restaurant.progress = function(complete) {
 		return 'warning';
 	}
 	return 'success';
-}
+};
+
+Template.restaurant.events({
+	'mousedown .media': function(evt) {
+		Router.setRestaurant(this._id);
+	}
+});
+
+
+/** BACKBONE **/
+
+var MainRouter = Backbone.Router.extend({
+	routes: {
+		':restaurant_id': 'main'
+	},
+	main: function(restaurant_id) {
+		var oldRestaurant = Session.get('restaurant_id');
+		if (oldRestaurant !== restaurant_id) {
+			Session.set('restaurant_id', restaurant_id);
+		}
+	},
+	setRestaurant: function(restaurant_id) {
+		this.navigate(restaurant_id, true);
+	}
+});
+
+Router = new MainRouter;
+
+Meteor.startup(function() {
+	Backbone.history.start({pushState: true});
+});
